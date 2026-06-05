6bce342469fdf93378f57ef707bd0d3ad0cd5774
littleroot7081-afk <littleroot7081@gmail.com>
2026-06-06 00:51:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function ScrollToTop() {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <CartProvider>
         <div className="min-h-screen flex flex-col bg-cream-100 font-body">
           <Header />
